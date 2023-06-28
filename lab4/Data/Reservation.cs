4da90cf0adcdf78980b8526e631c976cf35d38e0
Dawson <125399990+Daws98@users.noreply.github.com>
2023-06-28 14:12:57 -0600

    code

--- lab4/Data/Reservation.cs
+++ lab4/Data/Reservation.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace lab4.Data
 {
-    internal class Reservation
+    public class Reservation
     {
+        public string ReservationCode { get; set; }
+        public string FlightCode { get; set; }
+        public string Airline { get; set; }
+        public decimal Cost { get; set; }
+        public string Name { get; set; }
+        public string Citizenship { get; set; }
+        public bool IsActive { get; set; }
+
     }
 }
