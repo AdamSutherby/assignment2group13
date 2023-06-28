a70fb8c7d1950343c05a62a1181fae039af6ae56
Dawson <125399990+Daws98@users.noreply.github.com>
2023-06-28 15:25:10 -0600

    Reservation Changes

--- lab4/Data/Reservation.cs
+++ lab4/Data/Reservation.cs
@@ -16,5 +16,16 @@ namespace lab4.Data
         public string Citizenship { get; set; }
         public bool IsActive { get; set; }
 
+        public override string ToString()
+        {
+            string status = IsActive ? "Active" : "Inactive";
+            return $"Reservation Code: {ReservationCode}\n" +
+                   $"Flight Code: {FlightCode}\n" +
+                   $"Airline: {Airline}\n" +
+                   $"Cost: {Cost}\n" +
+                   $"Name: {Name}\n" +
+                   $"Citizenship: {Citizenship}\n" +
+                   $"Status: {status}";
+        }
     }
 }
