2d519c26b14c99c56caed97dcaa590065e145673
Dawson <125399990+Daws98@users.noreply.github.com>
2023-06-28 14:30:59 -0600

    Flights data with code

---
+++ lab4/Data/Flights.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4.Data
+{
+    public class Flights
+    {
+        public string FlightCode { get; set; }
+        public string Airline { get; set; }
+        public string Day { get; set; }
+        public string Time { get; set; }
+        public decimal Cost { get; set; }
+        public int SeatsAvailable { get; set; }
+    }
+}
